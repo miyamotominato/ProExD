969ce5e965dae316ffd2d2bdb7c87fc63c9fb02a
minato <c0a211211d@edu.teu.ac.jp>
2022-12-19 13:12:30 +0900

    修正後　#8

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -4,9 +4,6 @@ import sys
 
 
 def check_bound(obj_rct, scr_rct):
-    # 第1引数：こうかとんrectまたは爆弾rect
-    # 第2引数：スクリーンrect
-    # 範囲内：+1／範囲外：-1
     yoko, tate = +1, +1
     if obj_rct.left < scr_rct.left or scr_rct.right < obj_rct.right:
         yoko = -1
@@ -17,23 +14,19 @@ def check_bound(obj_rct, scr_rct):
 
 def main():
     clock =pg.time.Clock()
-    # 練習１
     pg.display.set_caption("逃げろ！こうかとん")
     scrn_sfc = pg.display.set_mode((1600, 900))
     scrn_rct = scrn_sfc.get_rect()
     pgbg_sfc = pg.image.load("fig/pg_bg.jpg")
     pgbg_rct = pgbg_sfc.get_rect()
 
-    # 練習３
     tori_sfc = pg.image.load("fig/6.png")
     tori_sfc = pg.transform.rotozoom(tori_sfc, 0, 2.0)
     tori_rct = tori_sfc.get_rect()
     tori_rct.center = 900, 400
-    # scrn_sfcにtori_rctに従って，tori_sfcを貼り付ける
     scrn_sfc.blit(tori_sfc, tori_rct) 
 
-    # 練習５
-    bomb_sfc = pg.Surface((20, 20)) # 正方形の空のSurface
+    bomb_sfc = pg.Surface((20, 20))
     bomb_sfc.set_colorkey((0, 0, 0))
     pg.draw.circle(bomb_sfc, (255, 0, 0), (10, 10), 10)
     bomb_rct = bomb_sfc.get_rect()
@@ -50,19 +43,14 @@ def main():
     bomb_rct2.centery = random.randint(0, scrn_rct.height)
     scrn_sfc.blit(bomb_sfc2, bomb_rct2) 
     vx2, vy2 = +3, +3
-    
-    
 
-    # 練習２
     while True:
         scrn_sfc.blit(pgbg_sfc, pgbg_rct) 
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 return
-        
 
-        # 練習4
-        key_dct = pg.key.get_pressed() # 辞書型
+        key_dct = pg.key.get_pressed()
         if key_dct[pg.K_UP]:
             tori_rct.centery -= 1
         if key_dct[pg.K_DOWN]:
@@ -81,8 +69,7 @@ def main():
             if key_dct[pg.K_RIGHT]:
                 tori_rct.centerx -= 1            
         scrn_sfc.blit(tori_sfc, tori_rct) 
-       
-        # 練習６
+
         bomb_rct.move_ip(vx, vy)
         scrn_sfc.blit(bomb_sfc, bomb_rct) 
         yoko, tate = check_bound(bomb_rct, scrn_rct)
@@ -95,7 +82,6 @@ def main():
         vx2 *= yoko2
         vy2 *= tate2
 
-        # 練習８
         if tori_rct.colliderect(bomb_rct) or tori_rct.colliderect(bomb_rct2):
             jikan = pg.time.get_ticks()
             fonto = pg.font.Font(None,80)
@@ -106,9 +92,6 @@ def main():
             scrn_sfc.blit(txt,(500,400))
             pg.display.update()
             clock.tick(0.5)
-            
-            pg.time.wait(3000)
-
             return
         pg.display.update()
         clock.tick(1000)
